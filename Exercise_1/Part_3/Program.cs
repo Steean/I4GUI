9f319be04497a5cf3b65c2a74b1b2d5f0259c20a
Steean <stian.fredsted@gmail.com>
2013-08-31 12:20:54 +0200

    Exercise 1
    All parts of the exercise is done

---
+++ Exercise_1/Part_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 0, b = 0, result = 0;
+            Console.Write("Enter 1 number: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter 2 number: ");
+            b = int.Parse(Console.ReadLine());
+
+            result = a + b;
+            Console.WriteLine("The sum of " + a + " and " + b + " is " + result);
+
+        }
+    }
+}
