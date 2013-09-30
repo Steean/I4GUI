98eced780db74b279fa9df7af654f02c588d72c4
Stian Fredsted Dahl <stian.fredsted@gmail.com>
2013-09-30 12:15:39 +0200

    Part 3-7 is all included in part 3.

---
+++ Exercise_5_Agent_2/Part_2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace Part_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt;
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            dt = new DispatcherTimer();
+            dt.Tick += dt_Tick;
+            dt.Interval = new TimeSpan(0,0,1);
+            dt.Start();
+
+
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            AgentClock.Text = DateTime.Now.ToString("F");
+        }
+
+        private void RedBg_Click(object sender, RoutedEventArgs e)
+        {           
+            mainGrid.Background = (Brush) this.Resources["redBrush"];
+        }
+
+        private void GreenBg_Click(object sender, RoutedEventArgs e)
+        {
+            mainGrid.Background = (Brush) this.Resources["greenBrush"];
+        }
+    }
+}
