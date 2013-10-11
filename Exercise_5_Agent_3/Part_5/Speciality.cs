4c65db1a61ee55ab6d2dbd9e60b7e7a8de02f9f1
Stian Fredsted Dahl <stian.fredsted@gmail.com>
2013-10-11 11:28:20 +0200

    Last part of part 5

---
+++ Exercise_5_Agent_3/Part_5/Speciality.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Part_5
+{
+    public class AgentSpeciality : ObservableCollection<string>
+    {
+        public AgentSpeciality()
+        {
+            Add("Assasination");
+            Add("Martini");
+            Add("Dying");
+        }
+    }
+}
